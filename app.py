e98b263ad57f994cd16bd92ed8bd431da5c69d5c
Dang Hoang Minh <43031977+danghoangminh@users.noreply.github.com>
2024-04-23 15:00:32 +0700

    feat: create version 3 for application

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 import platform
 import psutil
+import psycopg2
+from flask import request
 from flask import Flask
 from threading import Thread
+
+# Get the database name, username, and password from environment variables
+dbname = os.environ.get("POSTGRES_DB")
+username = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
 
 app = Flask(__name__)
 
@@ -21,15 +28,51 @@ def display_os_information():
     }
     return os_info
 
+@app.before_request
+def log_request_info():
+    # Set up a connection to the PostgreSQL database
+    conn = psycopg2.connect(
+        dbname=dbname,
+        user=username,
+        password=password,
+        host="postgres",
+        port="5432"
+    )
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # SQL query to insert data into the database
+    query = """
+    INSERT INTO logs (path, method, ip, time)
+    VALUES (%s, %s, %s, NOW())
+    """
+
+    cur.execute(query, (request.path, request.method, request.remote_addr))
+    conn.commit()
+    cur.close()
+
 @app.route('/health')
 def health_check():
-    if all_required_services_are_running():
+    if pgsql_check():
         return 'OK', 200
     else:
         return 'Service Unavailable', 500
 
-def all_required_services_are_running():
-    return True
+def pgsql_check():
+    try:
+        # Test the PostgreSQL connection
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=username,
+            password=password,
+            host="postgres",
+            port="5432"
+        )
+        conn.close()
+        return True
+    except psycopg2.OperationalError:
+        return False
 
 def run_flask():
     app.run(host='0.0.0.0',port=8080)
